bfbecd9f52d1ed5cd0bceaa194bb6c3a4e56daaa
Eli-IoT <eli.wauters@hotmail.com>
2025-06-03 23:57:15 +0200

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "JOUW_WIFI_NAAM";
+const char* WIFI_PASSWORD = "JOUW_WIFI_WACHTWOORD";
+const char* THINGSPEAK_API_KEY = "JOUW_API_KEY";
+
+#endif
